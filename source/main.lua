d443963dfa0439ed4b7ac8ff9e8a5962d03ef064
Juan Xavier Gomez <xendke@gmail.com>
2023-06-04 22:52:02 -0400

    initial commit

---
+++ source/main.lua
@@ -0,0 +1,62 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+local gfx <const> = playdate.graphics
+
+local playerSprite = nil
+
+local function myGameSetUp()
+    -- Set up the player sprite.
+    -- The :setCenter() call specifies that the sprite will be anchored at its center.
+    -- The :moveTo() call moves our sprite to the center of the display.
+
+    local playerImage = gfx.image.new("assets/images/hero")
+    assert( playerImage ) -- make sure the image was where we thought
+
+    playerSprite = gfx.sprite.new( playerImage )
+    playerSprite:moveTo( 200, 120 ) -- this is where the center of the sprite is placed; (200,120) is the center of the Playdate screen
+    playerSprite:add() -- This is critical!
+
+    -- We want an environment displayed behind our sprite.
+    -- Use setBackgroundDrawingCallback() to draw a background image.
+    local backgroundImage = gfx.image.new( "assets/images/background" )
+    assert( backgroundImage )
+
+    gfx.sprite.setBackgroundDrawingCallback(
+        function( x, y, width, height )
+            -- x,y,width,height is the updated area in sprite-local coordinates
+            -- The clip rect is already set to this area, so we don't need to set it ourselves
+            backgroundImage:draw( 0, 0 )
+        end
+    )
+
+end
+
+-- Now we'll call the function above to configure our game.
+-- After this runs (it just runs once), nearly everything will be
+-- controlled by the OS calling `playdate.update()` 30 times a second.
+
+myGameSetUp()
+
+-- `playdate.update()` is the heart of every Playdate game.
+-- This function is called right before every frame is drawn onscreen.
+-- Use this function to poll input, run game logic, and move sprites.
+
+function playdate.update()
+    if playdate.buttonIsPressed( playdate.kButtonUp ) then
+        playerSprite:moveBy( 0, -2 )
+    end
+    if playdate.buttonIsPressed( playdate.kButtonRight ) then
+        playerSprite:moveBy( 2, 0 )
+    end
+    if playdate.buttonIsPressed( playdate.kButtonDown ) then
+        playerSprite:moveBy( 0, 2 )
+    end
+    if playdate.buttonIsPressed( playdate.kButtonLeft ) then
+        playerSprite:moveBy( -2, 0 )
+    end
+
+    gfx.sprite.update()
+end
